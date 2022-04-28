79455ed6ced1e49b1209f8f5ec68b1374b0ca5a0
Pedro Costa <phmc99@outlook.com>
2022-04-28 01:55:16 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Router from "./routes"
+import "./styles/global.css"
+
+function App() {
+
+  return (
+    <>
+      <Router />
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Error from "../pages/Error";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/regiser" element={<Register />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+};
+
+export default Router;
